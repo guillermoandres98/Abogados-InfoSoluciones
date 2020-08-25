68a03b4acba7716145ab5cf416e7d6842c96da0f
guillermoandres98 <66839149+guillermoandres98@users.noreply.github.com>
2020-08-24 21:06:11 -0400

    primer commit de este proyecto

---
+++ FormularioRegistroContacto.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <!--Import Google Icon Font-->
+        <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+        <!--Import materialize.css-->
+        <link type="text/css" rel="stylesheet" href="css/materialize.min.css"  media="screen,projection"/>
+
+        <!--Let browser know website is optimized for mobile-->
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+    </head>
+
+    <body>
+        <div class="container center row">
+            <div class="col m6 s12 centralize form-contact">
+                <div role="form" class="wpcf7" id="wpcf7-f600-p22-o1" lang="pt-PT" dir="ltr">
+                    <div class="screen-reader-response"></div>
+                    <form action="/contato/#wpcf7-f600-p22-o1" method="post" class="wpcf7-form" novalidate="novalidate">
+                        <div style="display: none;">
+                            <input type="hidden" name="_wpcf7" value="600">
+                            <input type="hidden" name="_wpcf7_version" value="5.1.7">
+                            <input type="hidden" name="_wpcf7_locale" value="pt_PT">
+                            <input type="hidden" name="_wpcf7_unit_tag" value="wpcf7-f600-p22-o1">
+                            <input type="hidden" name="_wpcf7_container_post" value="22">
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <p>Seu nome<br><span class="wpcf7-form-control-wrap nome"><input type="text" name="nome" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></span></p>
+                                <p></p></div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <p>Seu email<br><span class="wpcf7-form-control-wrap email"><input type="email" name="email" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-email wpcf7-validates-as-required wpcf7-validates-as-email" aria-required="true" aria-invalid="false"></span></p>
+                                <p></p></div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <p>Seu Telefone<br><span class="wpcf7-form-control-wrap telefone"><input type="tel" name="telefone" value="" size="40" class="wpcf7-form-control wpcf7-text wpcf7-tel wpcf7-validates-as-required wpcf7-validates-as-tel" aria-required="true" aria-invalid="false" placeholder="(11) 1234-5678"></span></p>
+                                <p></p></div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <p>Assunto Relacionado<br><span class="wpcf7-form-control-wrap subject"><select name="subject" class="wpcf7-form-control wpcf7-select wpcf7-validates-as-required browser-default" aria-required="true" aria-invalid="false"><option value="">---</option><option value="Dúvidas sobre o produto">Dúvidas sobre o produto</option><option value="Dúvidas sobre preço">Dúvidas sobre preço</option><option value="Ajuda com o setor Financeiro">Ajuda com o setor Financeiro</option><option value="Quero migrar para a Eadbox">Quero migrar para a Eadbox</option><option value="Transmissão Ao vivo">Transmissão Ao vivo</option><option value="Ajuda de um Consultor">Ajuda de um Consultor</option><optgroup></optgroup></select></span></p>
+                                <p></p></div>
+                        </div>
+                        <div class="row">
+                            <div class="input-field col s12">
+                                <p>Mensagem<br><span class="wpcf7-form-control-wrap mensagem"><textarea name="mensagem" cols="40" rows="10" class="wpcf7-form-control wpcf7-textarea wpcf7-validates-as-required" aria-required="true" aria-invalid="false"></textarea></span></p>
+                                <p></p></div>
+                        </div>
+                        <div class="row" style="text-align:center;display:block;margin: 0 auto">
+                            <p class="waves-light btn-large orange darken-2 valign-wrapper" style="width:100%"><input type="submit" value="Enviar" class="wpcf7-form-control wpcf7-submit col btncontato"><span class="ajax-loader"></span> </p>
+                        </div>
+                                              </div></form></div>
+            </div>
+        </div>
+        <!--JavaScript at end of body for optimized loading-->
+        <script type="text/javascript" src="js/materialize.min.js"></script>
+    </body>
+</html>
